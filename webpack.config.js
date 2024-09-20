d83731e8ad64d10a279988c192156306d0f7cf90
Vasyl <vazhimov@gmail.com>
2024-09-20 16:26:36 +0300

    Организовал структуру файлов и добавил функционал информационного окна после сбора ключей

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,51 @@
+// webpack.config.js
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true, // Очистка папки dist перед сборкой
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader', // Транспиляция кода
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'], // Обработка CSS
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource', // Обработка изображений
-      },
-      {
-        test: /\.(mp3|wav)$/i,
-        type: 'asset/resource', // Обработка аудио
-      },
+    entry: './src/main.js', // Убедитесь, что этот путь корректен
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // Очистка папки dist перед сборкой
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // Транспиляция кода
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'], // Обработка CSS
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource', // Обработка изображений
+            },
+            {
+                test: /\.(mp3|wav)$/i,
+                type: 'asset/resource', // Обработка аудио
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // Шаблон HTML
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html', // Шаблон HTML
-    }),
-  ],
-  devServer: {
-    static: './dist',
-    hot: true, // Горячая перезагрузка
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
+    devServer: {
+        static: './dist',
+        hot: true, // Горячая перезагрузка
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
 };
